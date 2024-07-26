95078c39af2ea84773d57119f10929178d052fb6
kirti <sutharkirti494@gmail.com>
2024-07-26 23:39:49 +0530

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className=''>
+     <h1 className='text-lime-300 text-6xl text-center bg-black h-screen p-44'>Kirti</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
